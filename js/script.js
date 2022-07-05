a911f936400ffe8830238c39ed776b12e666ce55
Alexey Klok <klok.01@list.ru>
2022-07-05 22:56:31 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -98,4 +98,52 @@ window.addEventListener('DOMContentLoaded', () =>{
     }
 
     setClock('.timer', deadline);
+
+    // MODAl
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseButton = document.querySelector('[data-close]');
+ 
+    function openModal(){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    modalTrigger.forEach(btn =>{
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal (){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseButton.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e)=>{
+        if (e.target === modal){
+        closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e)=>{
+        if(e.code === "Escape" && modal.classList.contains('show')){
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll(){
+         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
